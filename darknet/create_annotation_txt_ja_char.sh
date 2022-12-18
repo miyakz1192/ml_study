7cff4fcb5aa38906fed7a4f24a384312e72f4b5a
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2022-12-18 16:26:26 +0000

    create_annotation_txt_ja_char.sh

--- darknet/create_annotation_txt_ja_char.sh
+++ darknet/create_annotation_txt_ja_char.sh
@@ -1,8 +1,6 @@
 
-template="0 0.07875 0.07875 0.1125 0.1125"
-
 ls *.jpg | while read line
 do
-	filename=`echo ${line} | sed -e 's/.jpg/.txt/'`
-	echo ${template} > ${filename}
+	filename=`echo ${line} | sed -e 's/.jpg/.xml/'`
+	cp /tmp/master.xml ${filename}
 done
